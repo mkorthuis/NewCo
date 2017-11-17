11ad4725ad41ee1cab94b2eaae42633f835435ba
mkorthuis <michael.korthuis@gmail.com>
2017-11-16 21:23:05 -0500

    hooked up the free quote page

--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {
+  Component
+} from 'react'
 import {
   FormGroup,
   HelpBlock,
   FormControl,
   ControlLabel
 } from 'react-bootstrap';
+import {
+  connect
+} from 'react-redux'
+import {
+  updateUser
+} from '../../reducers/User'
 
 function FieldGroup({
   id,
@@ -21,34 +29,61 @@ function FieldGroup({
   );
 }
 
+class SignUp extends Component {
+  handleInputChange = (evt) => {
+    evt.preventDefault();
+    this.props.updateUser({
+      [evt.target.id]: evt.target.value
+    });
+  }
 
-const SignUp = () => (
-  <div>
-    <h1>Enter your information to get a quote</h1>
-    <form>
-      <FieldGroup
-        id="firstName"
-        type="text"
-        placeholder="First Name"
-      />
-      <FieldGroup
-        id="lastName"
-        type="text"
-        placeholder="Last Name"
-      />
-      <FieldGroup
-        id="email"
-        type="text"
-        placeholder="Email"
-      />
-      <FieldGroup
-        id="phone"
-        type="text"
-        placeholder="Phone"
-      />
-    </form>
+  render() {
+    return (
+      <div>
+        <h1>Enter your information to get a quote</h1>
+        <form>
+          <FieldGroup
+            id="firstName"
+            type="text"
+            placeholder="First Name"
+            defaultValue={this.props.firstName} 
+            onChange={this.handleInputChange}
+          />
+          <FieldGroup
+            id="lastName"
+            type="text"
+            placeholder="Last Name"
+            defaultValue={this.props.lastName} 
+            onChange={this.handleInputChange}
+          />
+          <FieldGroup
+            id="email"
+            type="text"
+            placeholder="Email"
+            defaultValue={this.props.email} 
+            onChange={this.handleInputChange}
+          />
+          <FieldGroup
+            id="phone"
+            type="text"
+            placeholder="Phone"
+            defaultValue={this.props.phone} 
+            onChange={this.handleInputChange}
+          />
+        </form>
+      </div>
+    );
+  }
+}
 
-  </div>
-)
 
-export default SignUp
+export default connect(
+  (state) => ({
+    firstName: state.user.firstName,
+    lastName: state.user.lastName,
+    email: state.user.email,
+    phone: state.user.phone
+  }), {
+    updateUser
+  }
+)(SignUp)
